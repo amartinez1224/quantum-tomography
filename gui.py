acc53d15cb56011a700e321b2c6c822c4c202661
Andres Martinez <44838230+amartinez1224@users.noreply.github.com>
2023-04-30 15:53:28 -0500

    Added font size control for Plt

--- gui.py
+++ gui.py
@@ -550,10 +550,25 @@ def dataSimulator():
     window.wait_window()
     changeState(False)
      
+     
+def configureMatPlotLib():
+    SMALL_SIZE = 6
+    MEDIUM_SIZE = 8
+    BIGGER_SIZE = 9
+
+    plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+    plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+    plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+    plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+    plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+    plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
 if __name__ == "__main__":
     # ctypes.windll.shcore.SetProcessDpiAwareness(1) Windows only
     freeze_support() # Windows only
+    
+    configureMatPlotLib()
     
     win = tk.Tk()
     win.geometry('1125x900')
